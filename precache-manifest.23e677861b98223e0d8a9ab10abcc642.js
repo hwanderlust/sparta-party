30dda3f0ca953797dab8b5e27334a1fa6a9130ef
Paul <lpault29@gmail.com>
2018-12-12 00:59:19 -0500

    Updates

---
+++ precache-manifest.23e677861b98223e0d8a9ab10abcc642.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab28c3feef7bdabc5a3755ba1d9f67ff",
+    "url": "/sparta-party/static/media/col1row1.ab28c3fe.jpg"
+  },
+  {
+    "revision": "8c80ddc48627106af456",
+    "url": "/sparta-party/static/css/main.2bd0e722.chunk.css"
+  },
+  {
+    "revision": "54fb443974de59233c1b",
+    "url": "/sparta-party/static/js/1.54fb4439.chunk.js"
+  },
+  {
+    "revision": "ceacccd7e4e346552d10",
+    "url": "/sparta-party/static/js/runtime~main.ceacccd7.js"
+  },
+  {
+    "revision": "99f5a28e30f491f7d7f7aa156f7a93fa",
+    "url": "/sparta-party/static/media/slide1.99f5a28e.jpg"
+  },
+  {
+    "revision": "55c3338c52453b9f482014c63191a7d0",
+    "url": "/sparta-party/static/media/slide2.55c3338c.jpg"
+  },
+  {
+    "revision": "15238518f7eca6abd85d25e83c03ddbb",
+    "url": "/sparta-party/static/media/slide3.15238518.jpg"
+  },
+  {
+    "revision": "8c80ddc48627106af456",
+    "url": "/sparta-party/static/js/main.8c80ddc4.chunk.js"
+  },
+  {
+    "revision": "0ddb89632a026480545778bd6683e813",
+    "url": "/sparta-party/static/media/col2row1.0ddb8963.jpg"
+  },
+  {
+    "revision": "62d10abf394c72b9f4d2d2d2002796dd",
+    "url": "/sparta-party/static/media/col3row1.62d10abf.jpg"
+  },
+  {
+    "revision": "ea66207a789010190bb450f8e8702ae4",
+    "url": "/sparta-party/static/media/col1row2.ea66207a.jpg"
+  },
+  {
+    "revision": "fa71189b125c2ac002c29f1f693d0dd9",
+    "url": "/sparta-party/static/media/col2row2.fa71189b.jpg"
+  },
+  {
+    "revision": "44fa105e204334be833870234957c418",
+    "url": "/sparta-party/static/media/col3row2.44fa105e.jpg"
+  },
+  {
+    "revision": "cd478d3c5d44be004818fc57c7ff1070",
+    "url": "/sparta-party/index.html"
+  }
+];
